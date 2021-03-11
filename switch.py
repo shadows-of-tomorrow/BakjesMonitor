f53d630c076bcd93a830692520629603d2df74ed
robin@shadowsoftomorrow.io <robin@shadowsoftomorrow.io>
2021-03-11 10:53:01 +0100

    modified switch

--- switch.py
+++ switch.py
@@ -1,16 +1,28 @@
 import tinytuya
 
+CONFIG = {
+    "ID": "00042500c82b96020833",
+    "IP": "192.168.8.110",
+    "KEY": "c7bbe65e472792c9"
+}
+
+
 class Switch:
-    
+
     def __init__(self):
-        self.config = {"ip": "192.168.8.110", "id": "00042500c82b96020833", "key": "c7bbe65e472792c9"}
-        self.tuya = tinytuya.OutletDevice(self.config["id"], self.config["ip"], self.config["key"])
+        self.tuya = tinytuya.OutletDevice(CONFIG["ID"], CONFIG["IP"], CONFIG["KEY"])
         self.tuya.set_version(3.3)
         self.tuya.set_dpsUsed({"1": None})
-        
-    def _get_state(self):
-        return self.tuya.status()
-        
-if __name__ == "__main__":
-    switch = Switch()
-    print(switch._get_state())
+
+    def turn_on(self):
+        self.tuya.set_status(True)
+
+    def turn_off(self):
+        self.tuya.set_status(False)
+
+    def flip(self):
+        switch_state = self._get_switch_state()
+        self.tuya.set_status(not switch_state)
+
+    def _get_switch_state(self):
+        return self.tuya.status()['dps']['1']
