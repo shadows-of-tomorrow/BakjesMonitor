6a3f9d6360772eaece575b331d97cc770fe6a4ec
Robin van Kooten <robin@shadowsoftomorrow.io>
2021-04-10 11:55:11 +0200

    added requirements.txt

--- engine.py
+++ engine.py
@@ -24,24 +24,28 @@ class Engine:
         digits_old = self._get_digits()
         while True:
             time.sleep(self.step_time)
-            digits_new = self._get_digits()
-            print(digits_new)
-            if digits_new['top_left'] == digits_old['top_left'] or digits_new['top_right'] == digits_old['top_right']:
-                self.counter += 1
-                print("Screen frozen for " + str(self.counter * self.step_time) + " seconds.")
-                if self.counter >= 3:
-                    print("Turn alarm on!")
-                    self.switch.turn_on()
-                    time.sleep(0.1)
-                    print("Wait for " + str(self.wait_time) + " seconds...")
-                    time.sleep(self.wait_time)
-                    print("Turn alarm off!")
-                    time.sleep(0.1)
-                    self.switch.turn_off()
+            try:
+                digits_new = self._get_digits()
+                print(digits_new)
+                if digits_new['top_left'] == digits_old['top_left'] or digits_new['top_right'] == digits_old['top_right']:
+                    self.counter += 1
+                    print("Screen frozen for " + str(self.counter * self.step_time) + " seconds.")
+                    if self.counter >= 3:
+                        print("Turn alarm on!")
+                        self.switch.turn_on()
+                        time.sleep(0.1)
+                        print("Wait for " + str(self.wait_time) + " seconds...")
+                        time.sleep(self.wait_time)
+                        print("Turn alarm off!")
+                        time.sleep(0.1)
+                        self.switch.turn_off()
+                        self.counter = 0
+                else:
                     self.counter = 0
-            else:
-                self.counter = 0
-            digits_old = digits_new
+                digits_old = digits_new
+            except:
+                print("Could not read digits from screen..")
+                time.sleep(1.0)
             
         
 if __name__ == "__main__":
