79e39d0cac0afdf2381c5f47d55a0017395a2144
Robin van Kooten <robin@shadowsoftomorrow.io>
2021-05-29 13:06:59 +0200

    camera speed ups

--- hardware/camera.py
+++ hardware/camera.py
@@ -6,11 +6,14 @@ class Camera:
 
     def __init__(self, resolution=(512, 512)):
         self.resolution = resolution
+        self.camera = self._initialize_camera()
+
+    def _initialize_camera(self):
+        camera = picamera.PiCamera(resolution=self.resolution, framerate=90)
+        return camera
 
     def capture(self):
-        res = self.resolution
-        with picamera.PiCamera() as camera:
-            camera.resolution = res
-            img = np.empty((res[1], res[0], 3), dtype=np.uint8)
+        with self.camera as camera:
+            img = np.empty((self.resolution[1], self.resolution[0], 3), dtype=np.uint8)
             camera.capture(img, 'bgr')
             return img
